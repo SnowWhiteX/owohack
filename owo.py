c0bdba4bdaee5c68bbc324773fb5054de4682c2d
Snow WhiteX <80204080+SnowWhiteX@users.noreply.github.com>
2021-12-23 19:06:47 +0300

    owo.py

---
+++ owo.py
@@ -0,0 +1,47 @@
+import pyautogui
+import time
+import keyboard
+time.sleep(3)
+pyautogui.write("owo bot hack is starting...")
+time.sleep(3)
+pyautogui.press("enter")
+pyautogui.write("importing library...")
+time.sleep(3)
+pyautogui.press("enter")
+pyautogui.write("successful")
+time.sleep(3)
+pyautogui.press("enter")
+time.sleep(3)
+while True:
+    if keyboard.is_pressed("q"):
+        exit()
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    pyautogui.write("owo sell all")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owo cf 1")
+    pyautogui.press("enter")
+    time.sleep(7.90)
+    pyautogui.write("owo cf 10")
+    pyautogui.press("enter")
+    time.sleep(7.90)
+    pyautogui.write("owoh")
+    pyautogui.write("owo sell all")
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    pyautogui.write("owoh")
+    pyautogui.press("enter")
+    time.sleep(7.31)
+    
+    
+    
+    
+    pyautogui.press("enter")
+    pyautogui.write("owo sell all")
+    pyautogui.press("enter")
+    time.sleep(7.31)
